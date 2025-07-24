5f1a9f76a121aa34d5fb52a06a1f3e94e4cb7e84
yanbo92 <yanbo92@139.com>
2025-07-24 20:19:43 +0800

    [DELETE]remove toggle btn

--- assets/layouts/default.vue
+++ assets/layouts/default.vue
@@ -21,15 +21,7 @@
         </Splitpanes>
       </Pane>
     </Splitpanes>
-    <label
-      class="btn btn-circle swap bg-base-content/10 swap-rotate border-base-content/20 hover:border-primary fixed bottom-4 -left-12 w-16 shadow-sm transition-all hover:-left-4"
-      :class="{ '-left-6!': collapseNav }"
-      v-if="!isMobile && !forceMenuHidden"
-    >
-      <input type="checkbox" v-model="collapseNav" />
-      <mdi:chevron-right class="swap-on" />
-      <mdi:chevron-left class="swap-off" />
-    </label>
+
   </div>
   <dialog ref="modal" class="modal bg-base-300/50! items-start backdrop-blur-md transition-none!" @close="open = false">
     <div class="modal-box max-w-2xl bg-transparent pt-20 shadow-none">
